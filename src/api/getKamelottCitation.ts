beec774c482606c970495892e3368bb19524d79b
Alexis Capdepon <capdepon.alexis@gmail.com>
2022-05-28 18:00:43 +0200

    add specific funct for character

--- src/api/getKamelottCitation.ts
+++ src/api/getKamelottCitation.ts
@@ -6,3 +6,7 @@ const config: Config = require('../config/config.json');
 export const getCitation = async () => {
     return axios(config.url);
 }
+
+export const getCitationFromCharacter = async (character: string) => {
+    return axios(config.url + '/personnage/' + character );
+}
--- src/commands/Citation.ts
+++ src/commands/Citation.ts
@@ -1,14 +1,13 @@
 import { BaseCommandInteraction, Client } from "discord.js";
 import { Command } from "../Command";
-import { getCitation } from '../api/getKamelottCitation'
+import { getCitation, getCitationFromCharacter } from '../api/getKamelottCitation'
 
 export const Citation: Command = {
     name: "citation",
     description: "get random citation",
     type: "CHAT_INPUT",
     run: async (client: Client, interaction: BaseCommandInteraction) => {
-
-        const content = await getCitation()
+        const content = await getCitation();
         await interaction.followUp({
             ephemeral: true,
             content: content.data.citation.citation,
